048d184a70e5269d9729d8c9d96686449d9dd134
Muyiwa Obaremi <obaremimuyiwa@gmail.com>
2024-07-23 15:15:51 +0100

    func 2sum

--- golang/twosum.go
+++ golang/twosum.go
@@ -3,3 +3,14 @@ package main
 import (
 	"fmt"
 )
+func twoSum(nums []int, target int) []int {
+	numMap := make(map[int]int)
+	for i, num := range nums {
+		complement := target - num
+		if index, ok := numMap[complement]; ok {
+			return []int{index, i}
+		}
+		numMap[num] = i
+	}
+	return nil
+}
